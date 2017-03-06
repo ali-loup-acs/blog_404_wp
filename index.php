ad166202accd3232f788514643217d1e53a1169b
Ali LOUP <ali.j@codeur.online>
2017-03-06 10:39:14 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- jQuery library -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="assets/js/js.js"></script>
+    <!-- ************************** script creation zoneFlex404 ************************** -->
+
+
+    <title>Blog 404 Code Found</title>
+</head>
+
+<body id="body_home" onload="constructHeader();">
+
+  <div id="zoneSpeHome">
+
+		<!-- ************************** zone de portraits ************************** -->
+
+	</div>
+
+<?php include('template/header.html') ?>
+
+    <main id="main_home" class="container">
+        <section class="row">
+            <header class="col-md-12">
+                <h2 class="col-md-8">A la Une</h2>
+
+                <div class="bg_input col-md-4 hidden-xs hidden-sm pull-right">
+                    <input type="text" name="author" value="" placeholder="Nom de l'auteur">
+                    <input type="text" name="category" value="" placeholder="Nom de la catégorie">
+                </div>
+
+            </header>
+
+            <div class="clearfix"></div>
+
+            <div class="bg_article row-mas">
+
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+                <article class="item">
+                    <h3>Maybe not today !</h3>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <p>Oh, what wouldn’t I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face We interrupt this program to annoy you and make things generally irritating. Listen, strange women lyin’ in
+                        ponds distributin’ swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. I told you to lay off the beans, you whore! Manacles! Ooooh,
+                        my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think the sun shines out your ass, sonny. I cut down trees, I skip and jump, I like to press wildflowers. I put own womens’ clothing, and
+                        hang around in bars.</p>
+                    <h2>nom de l'auteur</h2>
+                    <a href="" class="pull-right">Read more</a>
+                </article>
+
+            </div>
+            <span class="">
+                      <a href="#" class="bg_chevron"><i class="fa fa-chevron-circle-left fa-3x"></i></a>
+                    </span>
+            <span class="pull-right">
+                      <a href="#" class="bg_chevron"><i class="fa fa-chevron-circle-right fa-3x"></i></a>
+                    </span>
+
+        </section>
+    </main>
+    
+<?php include('template/footer.html') ?>
+
+</body>
+
+</html>
